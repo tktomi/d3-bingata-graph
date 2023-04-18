6830acb9b87d2b8d78902322e63e78b056761693
tktomi <taketomi.tak@gmail.com>
2023-04-18 21:36:53 +0900

    フォルダ構成

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# データの扱いについて
-1. インポートした"bg.csv"を下記のコマンドでjsonにする
-```python
-python3 json_converter.py
-```
 
-2. jsonをD3.jsで可視化できるフォーマットにするために下記コマンドを実行する
-```python
+# Installation & usage
+
+## convert JSON file
+
+1. インポートした"bg.csv"を下記のコマンドでjsonにする
+
+```sh
+cd data_tools
+python3 json_converter.py
 node_links_converter.py
+mv node_links.json ../assets/data/node_links.json
 ```
 
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,18 +1,20 @@
-// グラフのデータを定義します
-var nodes = [
-  { id: 0, image: "./img/bg1.png" },
-  { id: 1, image: "./img/bg2.png" },
-  { id: 2, image: "./img/bg3.png" },
-];
-var links = [
-  { source: 0, target: 1 },
-  { source: 1, target: 2 },
-  { source: 2, target: 0 },
-];
+// グラフのデータを定義
+// var nodes = [
+//   { id: 0, image: "./img/bg1.png" },
+//   { id: 1, image: "./img/bg2.png" },
+//   { id: 2, image: "./img/bg3.png" },
+// ];
+// var links = [
+//   { source: 0, target: 1 },
+//   { source: 1, target: 2 },
+//   { source: 2, target: 0 },
+// ];
 
 // drawNetwork(nodes, links);
 
 function drawNetwork(nodes, links) {
+  const svgWidth = 1000;
+  const svgHeight = 800;
   const linkIds = links.map(link => [link.source, link.target]).flat();
   const nodeIds = nodes.map(node => node.id);
   const sub = nodeIds.filter(i => linkIds.indexOf(i) == -1)
@@ -27,7 +29,7 @@ function drawNetwork(nodes, links) {
   var simulation = d3.forceSimulation(nodes)
     .force("link", d3.forceLink().id(d => d.id).links(links))
     .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(250, 250));
+    .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2));
 
   // ノードをドラッグできるようにする関数を定義します
   function drag(simulation) {
@@ -51,14 +53,15 @@ function drawNetwork(nodes, links) {
     return d3.drag()
       .on("start", dragstarted)
       .on("drag", dragged)
-      .on("end",dragended);
+      .on("end", dragended);
   }
 
   // SVG要素を作成します
   var svg = d3.select("#myGraph")
     .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .style("background-color", "#555555");
   // var a = d3.select("#myGraph");
   // console.log(a);
   // var b = a.append("svg");
@@ -68,41 +71,44 @@ function drawNetwork(nodes, links) {
   // var svg = c.attr("height", 500);
   // console.log(svg);
 
-    const nodeSize = 50;
-  // ノードを描画するためのグループを作成します
-  var node = svg.append("g")
-    .selectAll("image")
-    .data(nodes)
-    .enter()
-    .append("image")
-    .attr("xlink:href", function(d, i) {
-      return d.image;
-    })
-    .attr("width", nodeSize)
-    .attr("height", nodeSize)
-    .call(drag(simulation)); // ノードをドラッグできるようにします
-
   // リンクを描画するための線を作成します
   var link = svg.append("g")
     .selectAll("line")
     .data(links)
     .enter()
     .append("line")
-    .attr("stroke", "black");
+    .attr("stroke", "white")
+    .attr("stroke-opacity", "20%");
+
+  const nodeSize = 20;
+  // ノードを描画するためのグループを作成します
+  var node = svg.append("g")
+    .selectAll("image")
+    .data(nodes)
+    .enter()
+    .append("image")
+    .attr("xlink:href", function (d, i) {
+      return `./assets/data/img/${d.image}`;
+    })
+    .attr("width", nodeSize)
+    .attr("height", nodeSize)
+    .call(drag(simulation)); // ノードをドラッグできるようにします
+
+
 
   // シミュレーションのフレームごとに呼び出される関数を定義します
-  simulation.on("tick", function() {
+  simulation.on("tick", function () {
     // ノードの座標を更新します
-    node.attr("x", function(d) { return d.x - nodeSize / 2; })
-      .attr("y", function(d) { return d.y - nodeSize / 2; });
+    node.attr("x", function (d) { return d.x - nodeSize / 2; })
+      .attr("y", function (d) { return d.y - nodeSize / 2; });
     // リンクの座標を更新します
-    link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+    link.attr("x1", function (d) { return d.source.x; })
+      .attr("y1", function (d) { return d.source.y; })
+      .attr("x2", function (d) { return d.target.x; })
+      .attr("y2", function (d) { return d.target.y; });
   });
 }
 
-d3.json("./node_links.json").then(function (data) {
+d3.json("./assets/data/node_links.json").then(function (data) {
   drawNetwork(data.nodes, data.links);
 });
