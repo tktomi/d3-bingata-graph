ea5071d7678ac17b4932eb3fe53dbad3a08e9914
Katsuya Endoh <studio@enkatsu.org>
2023-04-23 02:17:15 +0900

    add remove link & add link color

--- data_tools/node_links_converter.py
+++ data_tools/node_links_converter.py
@@ -16,12 +16,14 @@
         {
             "id": 1,
             "source": 1,
-            "target": 2
+            "target": 2,
+            "name": "松"
         },
         {
             "id": 1,
             "source": 3,
-            "target": 4
+            "target": 4,
+            "name": "松"
         },
         // ...
     ]
@@ -30,6 +32,7 @@
 
 import json
 import itertools
+import colorsys
 
 
 def pattern_and_bingata2links(pattern, bingata):
@@ -39,9 +42,18 @@ def pattern_and_bingata2links(pattern, bingata):
     has_pattern_bingata = filter(lambda b: pattern['id'] in b['patterns'], bingata)
     has_pattern_bingata_ids = list(map(lambda b: b['id'], has_pattern_bingata))
     links = itertools.combinations(has_pattern_bingata_ids, 2)
+    
+    def pattern_id2rcolor(pattern_id):
+        rgb = colorsys.hsv_to_rgb(pattern['id'] / 90.0, 1.0, 1.0)
+        print(rgb)
+        return [rgb[0] * 255, rgb[1] * 255, rgb[2] * 255]
+
     links = map(lambda l: {
         'source': int(l[0]),
         'target': int(l[1]),
+        'pattern_name': pattern['name'],
+        'pattern_id': pattern['id'],
+        'color': pattern_id2rcolor(pattern['id']),
     }, links)
     links = list(links)
     return links
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,37 +1,14 @@
-// グラフのデータを定義
-// var nodes = [
-//   { id: 0, image: "./img/bg1.png" },
-//   { id: 1, image: "./img/bg2.png" },
-//   { id: 2, image: "./img/bg3.png" },
-// ];
-// var links = [
-//   { source: 0, target: 1 },
-//   { source: 1, target: 2 },
-//   { source: 2, target: 0 },
-// ];
+function drawNetwork(nodes, links, patterns) {
+  const svgWidth = 600;
+  const svgHeight = 600;
 
-// drawNetwork(nodes, links);
-
-function drawNetwork(nodes, links) {
-  const svgWidth = 1000;
-  const svgHeight = 800;
-  const linkIds = links.map(link => [link.source, link.target]).flat();
-  const nodeIds = nodes.map(node => node.id);
-  const sub = nodeIds.filter(i => linkIds.indexOf(i) == -1)
-  console.log(sub);
-
-
-  console.log([...new Set(linkIds)].sort());
-  console.log([...new Set(nodeIds)].sort());
-
-
-  // 力学的グラフを作成します
+  // 力学的グラフを作成する
   var simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink().id(d => d.id).links(links))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2));
+    .force('link', d3.forceLink().id(d => d.id).links(links))
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(svgWidth / 2, svgHeight / 2));
 
-  // ノードをドラッグできるようにする関数を定義します
+  // ノードをドラッグできるようにする関数を定義する
   function drag(simulation) {
     function dragstarted(event, d) {
       if (!event.active) simulation.alphaTarget(0.3).restart();
@@ -51,64 +28,115 @@ function drawNetwork(nodes, links) {
     }
 
     return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
+      .on('start', dragstarted)
+      .on('drag', dragged)
+      .on('end', dragended);
   }
 
-  // SVG要素を作成します
-  var svg = d3.select("#myGraph")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight)
-    .style("background-color", "#555555");
-  // var a = d3.select("#myGraph");
-  // console.log(a);
-  // var b = a.append("svg");
-  // console.log(b);
-  // var c = b.attr("width", 500);
-  // console.log(c);
-  // var svg = c.attr("height", 500);
-  // console.log(svg);
+  // SVG要素を作成する
+  var svg = d3.select('#myGraph')
+    .append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight)
+    .style('background-color', '#555555');
 
-  // リンクを描画するための線を作成します
-  var link = svg.append("g")
-    .selectAll("line")
+  // リンクを描画するための線を作成する
+  var link = svg.append('g')
+    .selectAll('line')
     .data(links)
     .enter()
-    .append("line")
-    .attr("stroke", "white")
-    .attr("stroke-opacity", "20%");
+    .append('line')
+    .attr('stroke', 'white')
+    // linkの色分けする際に使用する
+    // .attr('stroke', l => `rgb(${l.color[0]}, ${l.color[1]}, ${l.color[2]})`)
+    .attr('stroke-opacity', '20%');
 
   const nodeSize = 20;
-  // ノードを描画するためのグループを作成します
-  var node = svg.append("g")
-    .selectAll("image")
+  // ノードを描画するためのグループを作成する
+  var node = svg.append('g')
+    .selectAll('image')
     .data(nodes)
     .enter()
-    .append("image")
-    .attr("xlink:href", function (d, i) {
+    .append('image')
+    .attr('xlink:href', function (d, i) {
       return `./assets/data/img/${d.image}`;
     })
-    .attr("width", nodeSize)
-    .attr("height", nodeSize)
-    .call(drag(simulation)); // ノードをドラッグできるようにします
+    .attr('width', nodeSize)
+    .attr('height', nodeSize)
+    .call(drag(simulation)); // ノードをドラッグできるようにする
 
-
-
-  // シミュレーションのフレームごとに呼び出される関数を定義します
-  simulation.on("tick", function () {
-    // ノードの座標を更新します
-    node.attr("x", function (d) { return d.x - nodeSize / 2; })
-      .attr("y", function (d) { return d.y - nodeSize / 2; });
-    // リンクの座標を更新します
-    link.attr("x1", function (d) { return d.source.x; })
-      .attr("y1", function (d) { return d.source.y; })
-      .attr("x2", function (d) { return d.target.x; })
-      .attr("y2", function (d) { return d.target.y; });
+  // シミュレーションのフレームごとに呼び出される関数を定義する
+  simulation.on('tick', function () {
+    // ノードの座標を更新する
+    node.attr('x', function (d) { return d.x - nodeSize / 2; })
+      .attr('y', function (d) { return d.y - nodeSize / 2; });
+    // リンクの座標を更新する
+    link.attr('x1', function (d) { return d.source.x; })
+      .attr('y1', function (d) { return d.source.y; })
+      .attr('x2', function (d) { return d.target.x; })
+      .attr('y2', function (d) { return d.target.y; });
   });
 }
 
-d3.json("./assets/data/node_links.json").then(function (data) {
-  drawNetwork(data.nodes, data.links);
+/**
+ * 柄の情報からチェックボックスのリストを生成する
+ */
+function patterns2checkboxList(patterns, change = () => {}) {
+  const checkboxList = patterns.map(pattern => {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = `link-checkbox-${pattern.id}`;
+    checkbox.classList.add(`link-checkbox`);
+    checkbox.value = pattern.id;
+    checkbox.setAttribute('checked', true);
+    checkbox.addEventListener('change', change);
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `link-checkbox-${pattern.id}`);
+    label.innerText = pattern.name;
+
+    const li = document.createElement('li');
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    return li;
+  })
+
+  const ul = document.createElement('ul');
+  ul.id = 'link-check-list';
+  checkboxList.forEach(checkboxLi => {
+    ul.appendChild(checkboxLi);
+  });
+
+  return ul;
+}
+
+Promise.all([
+  d3.json('./assets/data/bg.json'),
+  d3.json('./assets/data/node_links.json')
+]).then(data => {
+  const bingata = data[0];
+  const node_links = data[1];
+
+  const checkboxList = patterns2checkboxList(bingata.patterns, checkbox => {
+    d3.select('svg').remove();
+    const checkboxes = Array.from(document.getElementsByClassName('link-checkbox'));
+    const checkedIds = checkboxes
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => Number(checkbox.value));
+    const filteredLinks = node_links.links
+      .filter(l => checkedIds.find(checkedId => checkedId === l.pattern_id));
+    node_links.nodes.forEach(node => {
+      node.x = 0.0;
+      node.y = 0.0;
+      node.vx = 0.0;
+      node.vy = 0.0;
+    });
+    drawNetwork(node_links.nodes, filteredLinks, bingata.patterns);
+  });
+
+  d3.select('#myGraph')
+    .node()
+    .appendChild(checkboxList);
+  
+  drawNetwork(node_links.nodes, node_links.links, bingata.patterns);
 });
